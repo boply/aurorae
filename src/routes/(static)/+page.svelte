92d9b71224b577d152f351cdd574269dbfe4a4d6
boply <braedencopley@gmail.com>
2023-04-25 23:52:38 -0700

    navbar and stuff

---
+++ src/routes/(static)/+page.svelte
@@ -0,0 +1,4 @@
+<div class="px-8 py-24 flex bg-base-100 h-full w-full ">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>

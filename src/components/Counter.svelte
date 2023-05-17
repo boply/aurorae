0fc8fffbb1c9e7ea302abd7a2b597a719b5c3216
boply <braedencopley@gmail.com>
2023-05-17 10:57:15 -0700

    counter and card

---
+++ src/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    let number = 0;
+
+
+</script>
+
+<div class="flex flex-row w-40 h-12">
+    <div class="btn btn-secondary w-1/3">
+        <button id="minus-btn">-</button>
+    </div>
+    <div class="box-rounded bg-red-500 w-1/3 h-full flex text-center">
+        <p class="flex self-center">{number}</p>
+    </div>
+    <div class="btn btn-secondary w-1/3">
+        <button id="plus-btn">+</button>
+    </div>
+</div>

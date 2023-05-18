280b80af36a76d3eb85f4089c4f85b8cfc5e285b
kar722 <karthikv722@gmail.com>
2023-05-18 09:00:48 -0700

    increment and decrement

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,18 +1,29 @@
-<script>
+<script lang="ts">
+    let count = 0;
 
-    let number = 0;
-
-
+    function increment() {
+        count++;
+    }
+    function decrement() {
+        if (count > 0){
+            count--;
+        }
+    }
 </script>
 
 <div class="flex flex-row w-40 h-12">
-    <div class="btn btn-secondary w-1/3">
-        <button id="minus-btn">-</button>
+    <div class="btn btn-secondary w-1/3" on:click={decrement}>
+        <button id="minus-btn">
+            -
+        </button>
     </div>
     <div class="box-rounded bg-500 w-1/3 h-full flex flex-col text-center flex-row">
-        <p>{number}</p>
+        <p>{count}</p>
     </div>
-    <div class="btn btn-secondary w-1/3">
-        <button id="plus-btn">+</button>
+    <div class="btn btn-secondary w-1/3" on:click={increment}>
+        
+        <button id="plus-btn">
+            +
+        </button>
     </div>
 </div>

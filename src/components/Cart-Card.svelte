7ca33423b46019adcc42d8e1c0c7f3f9337a9fa6
boply <braedencopley@gmail.com>
2023-05-19 09:52:38 -0700

    cart

---
+++ src/components/Cart-Card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    import Counter from "./Counter.svelte";
+
+    export let title: string;
+    export let description: string;
+    export let image: string;
+
+</script>
+
+<div class="card bg-base-100 shadow-xl max-h-96 h-fit w-full box">
+    <figure class="h-4 w-full"><img src={image} alt="picture" /></figure>
+    <div class="card-body">
+      <button class="btn btn-sm btn-circle btn-secondary absolute -right-2 -top-2">X</button>
+      <h2 class="card-title">{title}</h2>
+      <p>{description}</p>
+      <div class="card-actions justify-end flex flex-row">
+        <Counter/>
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
--- src/routes/(static)/cart/+page.svelte
+++ src/routes/(static)/cart/+page.svelte
@@ -1,3 +1,20 @@
-<div class="flex min-h-screen w-full px-8 py-24">
-    cart
+<script>
+
+    import Card from "../../../components/Cart-Card.svelte";
+
+</script>
+<div class="flex flex-col min-h-screen w-full p-24 justify-between">
+    <div class="flex full w-1/2 min-w-96 justify-center">
+        <Card title="Neural Stem Cells" description="Derived from iPSCs" image="https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2018/10/stem-cells.jpg?w=1155&h=1541" />
+    </div>
+
+    <div class="flex flex-col h-fit w-1/4 gap-2 self-end rounded-box shadow-xl bg-base-100 fixed">
+        <div class="flex w-full h-12 bg-secondary text-center">Cart</div>
+        <div class="flex flex-col p-8 gap-10">
+            <p>Products: $9899</p>
+            <p>Tax: $100</p>
+            <p>Subtotal: $9999</p>
+            <button class="btn btn-primary">Checkout Cart</button>
+        </div>
+    </div>
 </div>

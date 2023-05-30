c15aeab57d72d341e1af71f48ed89ee2d7468b10
SanjayB06 <sanjaymbharadwaj@gmail.com>
2023-05-30 14:22:57 -0700

    products page

--- src/routes/(static)/products/+page.svelte
+++ src/routes/(static)/products/+page.svelte
@@ -3,6 +3,7 @@
 	
   /** @type {import('./$types').PageData} */
   export let data: any;
+  let products = data.products;
   //let products = [data.products.products];
 
 </script>
@@ -11,7 +12,8 @@
 <div class="flex flex-wrap gap-12 min-h-screen w-full py-24 md:px-24 p-8 justify-center md:justify-normal">
   
   
-  {#each data as product}
+  {#each products as product}
+    
     <Card product={product} />
   {/each}
 

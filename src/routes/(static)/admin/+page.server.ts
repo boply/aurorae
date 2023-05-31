5d579512dc7dd3721d6896e40d2084d457e86ee0
boply <braedencopley@gmail.com>
2023-05-31 16:38:55 -0700

    fix tables

---
+++ src/routes/(static)/admin/+page.server.ts
@@ -0,0 +1,30 @@
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ fetch }) {
+    const res = await fetch(`http://localhost:8093/api/products/getProducts`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        //credentials: "include", // include, *same-origin, omit
+        headers: {
+            'Accept': 'application/json',
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+
+    },
+  });
+    const products: any[] = (await res.json())
+
+    const res2 = await fetch(`http://localhost:8093/api/contact/`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        //credentials: "include", // include, *same-origin, omit
+        headers: {
+            'Accept': 'application/json',
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+
+    },
+  });
+    const contacts: any[] = (await res2.json())
+    
+    return {products, contacts};
+}
--- src/routes/(static)/admin/+page.svelte
+++ src/routes/(static)/admin/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 
-import { user, aid } from '$firebase';
 const options = {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
@@ -11,6 +10,15 @@ const options = {
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
       };
+
+  /** @type {import('./$types').PageData} */
+  export let data: any;
+  let products = data.products.data;
+  let contacts = data.contacts;
+
+  console.log(products)
+  console.log("------")
+  console.log(contacts)
 
 const addProduct = () => {
   
@@ -35,10 +43,6 @@ const addProduct = () => {
 
 
 
-
-
-
-
 function removeProduct() {
   // java api creation
 }
@@ -47,163 +51,81 @@ function updateProduct() {
   // java api creation
 }
 
-const getContacts = () => {
 
-// Fetch data from API
-fetch('http://localhost:8093/api/contact/')
-.then(response => response.json())
-.then(data => {
-
-    let contactsData = data;
-    console.log(contactsData);
-    
-    // get row elements
-    let nameRow = document.getElementById("nameRows");
-    let idRow = document.getElementById("idRows");
-    let companyRow = document.getElementById("companyRows");
-    let messageRow = document.getElementById("messageRows");
-    let emailRow = document.getElementById("emailRows");
-    
-    // clear table contents
-    for (let j = 0; j < contactsData.length; j++){    
-
-        nameRow.innerHTML = " ";
-        idRow.innerHTML = " ";
-        companyRow.innerHTML = " ";
-        messageRow.innerHTML = " ";
-        emailRow.innerHTML = " ";
-
-    }
-
-    // add table contents
-    for (let i = 0; i < contactsData.length; i++){  
-
-        let header = document.createElement("th");
-        header.setAttribute("id", i);
-        header.innerHTML = contactsData[i].name;
-        nameRow.appendChild(header);
-
-        let newIdRow = document.createElement("td");
-        newIdRow.setAttribute("id", i);
-        newIdRow.innerHTML = "id: " + contactsData[i].id;
-        idRow.appendChild(newIdRow);
-
-        let newCompanyRow = document.createElement("td");
-        newCompanyRow.setAttribute("id", i);
-        newCompanyRow.innerHTML = contactsData[i].company;
-        companyRow.appendChild(newCompanyRow);
-
-
-        let newMessageRow = document.createElement("td");
-        newMessageRow.setAttribute("id", i);
-        newMessageRow.innerHTML = contactsData[i].message;
-        messageRow.appendChild(newMessageRow);  
-
-        let newEmailRow = document.createElement("td");
-        newEmailRow.setAttribute("id", i);
-        newEmailRow.innerHTML = contactsData[i].email;
-        emailRow.appendChild(newEmailRow);  
-    }
-
-});
-}
-
-const showProducts = () => {
-  fetch('http://localhost:8093/api/products/getProducts')
-.then(response => response.json())
-.then(data => {
-  console.log(data["data"]);
-  let table = document.getElementById("productsTable");
-  for (let i =0;i<data["data"].length;i++) {
-    var row = table.insertRow(1);
-    var prodID = row.insertCell(0);
-    var name = row.insertCell(1);
-    var price = row.insertCell(2);
-    var description = row.insertCell(3);
-    var instock = row.insertCell(4);
-    prodID.innerText = data["data"][i]["ID"];
-    name.innerText = data["data"][i]["NAME"];
-    price.innerText = data["data"][i]["PRICE"];
-    description.innerText = data["data"][i]["DESCRIPTION"];
-    if (data["data"][i]["INSTOCK"] == "0") {
-      instock.innerText = "no";
-    } else {
-      instock.innerText = "yes";
-    }
-    
-
-
-  }
-})
-
-}
-showProducts();
-
+  
 </script>
 
 
-<div class="flex min-h-screen w-full flex-col md:flex-row gap-10 px-8 py-24 justify-evenly">
+<div class="flex flex-col min-h-screen w-full gap-10 px-8 py-24">
   
-  <div class="w-full md:w-1/3 flex flex-col h-fit gap-8 self-center md:self-start">
+  <div class="flex w-full h-fit flex-col md:flex-row gap-8">
+  
+    <div class="w-full md:w-1/3 flex flex-col h-fit gap-8 self-center md:self-start">
 
-    <!-- Add Products -->
-    <div class="collapse bg-base-100 shadow-xl rounded-lg p-4 self-center md:self-start h-fit w-full">
-      <input type="checkbox" />
-      <div class="collapse-title text-xl font-medium text-center">
-        Add Product Panel
-      </div>
-      <div class="collapse-content flex flex-col gap-6">
-          <div class="form-control w-full">
-              <label class="label">
-              <span class="label-text">Product Name</span>
-              </label>
-              <input id = "prodName" type="text" placeholder="Ex: Induced Pluripotent Stem Cells, Normal" class="input input-bordered w-full" />
-          </div>
-          <div class="form-control">
-              <label class="label">
-                <span class="label-text">Description</span>
-              </label>
-              <textarea id = "prodDescrip" class="textarea textarea-bordered h-24" placeholder="Product Description"></textarea>
-            </div>
+      <!-- Add Products -->
+      <div class="collapse bg-base-100 shadow-xl rounded-lg p-4 self-center md:self-start h-fit w-full">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium text-center">
+          Add Product Panel
+        </div>
+        <div class="collapse-content flex flex-col gap-6">
             <div class="form-control w-full">
-              <label class="label">
-              <span class="label-text">Price ($)</span>
-              </label>
-              <input id="prodPrice" type="number" placeholder="Ex: 100.00" class="input input-bordered w-full" />
-          </div>
-          <input type="button" value="Add Product" class="btn btn-primary my-4 w-full" on:click={addProduct}>
+                <label class="label">
+                <span class="label-text">Product Name</span>
+                </label>
+                <input id = "prodName" type="text" placeholder="Ex: Induced Pluripotent Stem Cells, Normal" class="input input-bordered w-full" />
+            </div>
+            <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Description</span>
+                </label>
+                <textarea id = "prodDescrip" class="textarea textarea-bordered h-24" placeholder="Product Description"></textarea>
+              </div>
+              <div class="form-control w-full">
+                <label class="label">
+                <span class="label-text">Price ($)</span>
+                </label>
+                <input id="prodPrice" type="number" placeholder="Ex: 100.00" class="input input-bordered w-full" />
+            </div>
+            <input type="button" value="Add Product" class="btn btn-primary my-4 w-full" on:click={addProduct}>
+        </div>
       </div>
+
+      <!-- Remove Product -->
+      <div class="w-full bg-base-100 shadow-xl rounded-lg p-8 self-center md:self-start">
+        <input type="button" value="Remove Product" class="btn btn-primary h-full w-full" on:click={removeProduct}>
+      </div>
+
     </div>
 
-    <!-- Remove Product -->
-    <div class="w-full bg-base-100 shadow-xl rounded-lg p-8 self-center md:self-start">
-      <input type="button" value="Remove Product" class="btn btn-primary h-full w-full" on:click={removeProduct}>
+
+    <!-- Table -->
+    <div class="overflow-x-auto w-full md:w-2/3 self-center md:self-start bg-base-100 shadow-xl rounded-lg">
+      <table id="productsTable" class="table w-full">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>In Stock?</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each products as product}
+          <tr>
+            <td>{product.ID}</td>
+            <td>{product.NAME}</td>
+            <td>{product.DESCRIPTION}</td>
+            <td>{product.PRICE}</td>
+            <td>{product.INSTOCK}</td>
+          </tr>
+          {/each}
+        </tbody>
+      </table>
     </div>
-
-    
-
-
-</div>
-
-
-  <!-- Table -->
-  <div class="overflow-x-auto w-full md:w-2/3 self-center md:self-start bg-base-100 shadow-xl rounded-lg">
-    <table id="productsTable" class="table w-full">
-      <!-- head -->
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Price</th>
-          <th>In Stock?</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr></tr>
-      </tbody>
-    </table>
   </div>
 
   <!-- View Contact Forms-->
@@ -213,24 +135,28 @@ showProducts();
       Contacts
     </div>
     <div class="collapse-content flex flex-col gap-6">
-      <button class="btn" on:click={getContacts}>Get Latest Data</button>
-      <table>
-
-        <tr id="nameRows">
-        </tr>
-      
-        <tr id="idRows">
-        </tr>
-      
-        <tr id="companyRows">
-        </tr>
-      
-        <tr id="messageRows">
-        </tr>
-      
-        <tr id="emailRows">
-        </tr>
-      
+      <button class="btn btn-primary">Get Latest Data</button>
+      <table id="productsTable" class="table w-full">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Name</th>
+            <th>Message</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each contacts as contact}
+          <tr>
+            <td>{contact.email}</td>
+            <td>{contact.company}</td>
+            <td>{contact.name}</td>
+            <td>{contact.message}</td>
+          </tr>
+          {/each}
+        </tbody>
       </table>
     </div>
   </div>

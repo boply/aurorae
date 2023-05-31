207fb57082ed1cd7aa0d36c8d3952c6e5292b1ea
kar722 <karthikv722@gmail.com>
2023-05-30 18:44:29 -0700

    contact page is working

--- src/routes/(static)/contact/+page.svelte
+++ src/routes/(static)/contact/+page.svelte
@@ -10,37 +10,55 @@
             // 'Content-Type': 'application/x-www-form-urlencoded', 
             },
         };
-    
-        function send(){
-    var myVar = {"id" : 36};
-    console.log("peopleData", document.getElementById('id').value);
-    fetch("http://localhost:8093/api/contact/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "text/plain"
-        },
-        body: JSON.stringify(myVar)
-    }).then(function(response) {
-        return response.text();
-    }).then(function(muutuja){
-        document.getElementById('väljund').innerHTML = muutuja;
-    });
-}
-    const addPeople = () => {
-      var email = document.getElementById("inputEmail").value;
-      var company = document.getElementById("inputCompany").value;
-      var name = document.getElementById("inputName").value;
-      var message = document.getElementById("inputMsg").value;
-      const options = {
-        method: 'POST',
-      };
-      fetch( 'http://localhost:8093/api/contact/post/' + "?email=" + email + "&company=" + company + "&name=" + name + "&message=" + message , options )
-      .then(response => response.json())
-      .then(data => {console.log(data);});
-}
+
+    const contact = () => {
+        
+        var email = document.getElementById("inputEmail").value;
+        var company = document.getElementById("inputCompany").value;
+        var name = document.getElementById("inputName").value;
+        var message = document.getElementById("inputMsg").value;
+
+        const options = {
+            method: 'POST',
+        };
+        fetch( 'http://localhost:8093/api/contact/post/' + "?email=" + email + "&company=" + company + "&name=" + name + "&message=" + message , options )
+        .then(response => response.json())
+        .then(data => {console.log(data);});
+
+        var success = document.getElementById("success");
+        success.style.display = "block";
+        // Set a timeout to hide the div after 3 seconds
+        setTimeout(function() {
+          success.style.display = "none";
+        }, 3000);
+        
+        // Store the visibility state in local storage
+        localStorage.setItem("isVisible", "true");
+
+        // Check if the div should be hidden or shown based on local storage
+        window.onload = function() {
+          var success = document.getElementById("success");
+          var isVisible = localStorage.getItem("isVisible");
+          
+          if (isVisible === "true") {
+            success.style.display = "block";
+          }
+        };
+
+
+        document.getElementById("inputEmail").value = "";
+        document.getElementById("inputCompany").value = "";
+        document.getElementById("inputName").value = "";
+        document.getElementById("inputMsg").value = "";
+
+
+    }
 
   </script>
   <div class="flex min-h-screen w-full px-8 py-24 justify-center">
+
+
+
     <!-- Other content -->
     <div class="isolate px-100 py-100 sm:py-200 lg:px-500">
         <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
@@ -50,7 +68,7 @@
           <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
           <p class="mt-2 text-lg leading-8 text-gray-600">Reach Out to the Aurorae Biolabs Team for Any Questions or Comments.</p>
         </div>
-        <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div class="mx-auto mt-16 max-w-xl sm:mt-20">
           <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div class="sm:col-span-2">
               <label class="block text-sm font-semibold leading-6 text-gray-900">Name</label>
@@ -72,15 +90,21 @@
             </div>
             <div class="sm:col-span-2">
               <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
-              <input type= "message">
               <div class="mt-2.5">
                 <textarea rows="4"  id="inputMsg" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
               </div>
             </div>
           </div>
           <div class="mt-10">
-            <button on:click={addPeople} class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+            <button on:click={contact} class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
           </div>
-        </form>   
-      </div>  
+          <br>
+          <div class="alert alert-success shadow-lg" id="success" style="display: none;">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <span>Your message has been sent!</span>
+            </div>
+          </div>
+        </div>   
+      </div>
   </div>

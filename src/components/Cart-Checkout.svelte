e3a093e862dc88857113d636129c5f284873f08e
boply <braedencopley@gmail.com>
2023-05-31 10:25:18 -0700

    checkout sum and tax

--- src/components/Cart-Checkout.svelte
+++ src/components/Cart-Checkout.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
 
-    export let product: string;
-    export let tax: string;
-    export let total: string;
+    let test = [
+        { id: 1, PRICE: 310.25, completed: true },
+        { id: 2, PRICE: 201.11, completed: true },
+        { id: 3, PRICE: 55.91, completed: false }
+    ];
+
+    function sumProducts( product:any ) {
+        sum += product.PRICE;
+    }
+
+    export let products: any;
+    let tax: number;
+    let total: number;
+
+    let sum: number = 0;
+
+    test.forEach(sumProducts);
+    tax = sum*0.08;
+    tax = Math.round( tax * 1e2 ) / 1e2;
+    total = sum+tax;
 
 </script>
 
@@ -10,7 +27,7 @@
     <p class="flex text-center self-center h-fit w-fit font-bold text-xl">Cart</p>
 </div>
 <div class="flex flex-col p-8 gap-8">
-    <p>Products: ${product}</p>
+    <p>Products: ${sum}</p>
     <p>Tax: ${tax}</p>
     <p>Total: ${total}</p>
     <a href="/checkout" class="btn btn-primary">Checkout Cart</a>
--- src/routes/(static)/cart/+page.svelte
+++ src/routes/(static)/cart/+page.svelte
@@ -2,13 +2,6 @@
 
     import Card from "$components/Card-Cart.svelte";
     import Checkout from "$components/Cart-Checkout.svelte";
-    // import { user } from "$firebase";
-
-    // let _user: any = $user;
-	
-    // user.subscribe((data) => {
-	//   _user = data;
-	// });
     
     /** @type {import('./$types').PageData} */
     export let data: any;
@@ -17,14 +10,14 @@
 </script>
 
 <div class="flex flex-row min-h-screen w-full py-24 px-8 lg:px-24">
-    <div class="flex flex-col lg:w-1/2 md:w-3/4 w-full justify-center gap-8">
+    <div class="flex flex-col lg:w-1/2 md:w-3/4 w-full justify-center gap-10">
         {#each products as product}
             <Card product={product} />
         {/each}
     </div>
 
     <div class="lg:flex flex-col h-fit max-h-3/4 w-96 gap-2 right-24 top-24 lg:self-end self-center rounded-box shadow-xl bg-base-100 fixed hidden">
-        <Checkout product="980" tax="19" total="999"/>
+        <Checkout products={products} /> 
     </div>
 
     <label for="my-modal-4" class="btn btn-primary lg:hidden fixed right-6 bottom-6">Checkout</label>
@@ -32,7 +25,7 @@
     <input type="checkbox" id="my-modal-4" class="modal-toggle" />
     <label for="my-modal-4" class="modal cursor-pointer">
         <label class="modal-box relative h-fit w-96 p-0" for="">
-            <Checkout product="980" tax="19" total="999"/>
+            <Checkout products={products} />
         </label>
     </label>
 </div>

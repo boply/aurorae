ab52060dc297d039f16412a32bc36429c8ab9643
boply <braedencopley@gmail.com>
2023-06-05 08:32:20 -0700

    idk

--- src/routes/(static)/cart/+layout.svelte
+++ src/routes/(static)/cart/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { user, authenticated } from '$firebase';
-    import { writable } from 'svelte/store';
     
     let _user: any;
     // export const uid = writable("")
@@ -10,9 +9,9 @@
         // uid.set(_user.uid)
     });
     
-    </script>
+</script>
     
-    {#if authenticated}
+    {#if $authenticated}
         <slot />
     {:else}
     
